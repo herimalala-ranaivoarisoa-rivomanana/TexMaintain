caa3ebe9e877a87d1af43412ef0abe4a5b30b6c2
rabaivoarisoa <rabaivoarisoa@gmx.fr>
2026-01-02 03:31:00 +0300

    fix url

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -4,7 +4,7 @@ import JSONbig from 'json-bigint';
 
 
 const localApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://texmaintain-server.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'https://texmaintain.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
